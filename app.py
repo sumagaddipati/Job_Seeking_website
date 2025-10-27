55c298581be2ecc16551f88b8d3d399d11ff414e
Sumalatha Gaddipati <165734884+sumagaddipati@users.noreply.github.com>
2025-10-27 09:30:30 +0000

    First Flask Project made

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.route("/")
+def suma():
+    return "suma is ambitious"
+    #return __name__
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
